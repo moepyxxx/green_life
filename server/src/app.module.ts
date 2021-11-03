18cf54bf5e95734685f5c74ae7b7d9d80792ecc7
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-11-04 00:13:28 +0900

    静的ファイルがローカルドメインでアクセスできるようにした

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,11 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 const url = `mongodb://root:example@mongo:27017/greenlife?authSource=admin`;
 
 @Module({
-  imports: [MongooseModule.forRoot(url), PostsModule],
+  imports: [
+    MongooseModule.forRoot(url),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'image'),
+    }),
+    PostsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

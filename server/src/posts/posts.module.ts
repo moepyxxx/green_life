ec53cab30d7e340fb44ec0d5d2833bbf40c675a4
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-11-03 14:32:50 +0900

    findAllでDB上のデータが返却されるようにした

--- server/src/posts/posts.module.ts
+++ server/src/posts/posts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { Post, PostSchema } from './post.schema';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }])],
   controllers: [PostsController],
   providers: [PostsService],
 })
--- server/src/posts/posts.service.ts
+++ server/src/posts/posts.service.ts
@@ -1,4 +1,7 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument } from './post.schema';
 import { IPost } from 'src/interfaces/post';
 
 interface IGetPostsRes {
@@ -6,75 +9,90 @@ interface IGetPostsRes {
   posts: IPost[]
 }
 
+interface ITestPost {
+  imagePath: string;
+  comment: string;
+}
+
 @Injectable()
 export class PostsService {
-  
+  constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
+
+  async create(testPost: ITestPost): Promise<Post> {
+    const createdPost = new this.postModel(testPost);
+    return createdPost.save();
+  }
+
+  async findAll(): Promise<Post[]> {
+    return this.postModel.find().exec();
+  }
+
   getPosts(page: number): IGetPostsRes {
     return {
       current: page ? page + 1 : 0,
       posts: [{
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_1.jpg',
+        imagePath: 'sample_1.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_2.jpg',
+        imagePath: 'sample_2.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_3.jpg',
+        imagePath: 'sample_3.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_4.jpg',
+        imagePath: 'sample_4.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_5.jpg',
+        imagePath: 'sample_5.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_6.jpg',
+        imagePath: 'sample_6.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_7.jpg',
+        imagePath: 'sample_7.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_1.jpg',
+        imagePath: 'sample_1.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_2.jpg',
+        imagePath: 'sample_2.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_3.jpg',
+        imagePath: 'sample_3.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_4.jpg',
+        imagePath: 'sample_4.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_5.jpg',
+        imagePath: 'sample_5.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_6.jpg',
+        imagePath: 'sample_6.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_7.jpg',
+        imagePath: 'sample_7.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_1.jpg',
+        imagePath: 'sample_1.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_2.jpg',
+        imagePath: 'sample_2.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_3.jpg',
+        imagePath: 'sample_3.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_4.jpg',
+        imagePath: 'sample_4.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_5.jpg',
+        imagePath: 'sample_5.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_6.jpg',
+        imagePath: 'sample_6.jpg',
       }, {
         id: Math.random().toString(32).substring(2),
-        image_path: 'sample_7.jpg',
+        imagePath: 'sample_7.jpg',
       }]
     }
   }

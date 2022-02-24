cf2edbdeaa9e8cfd2209e5ca4c1f164443520022
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-02-24 10:57:20 +0900

    TOPのレイアウトを変更した

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -12,104 +12,32 @@ export default function Home({ page, posts }: InferGetStaticPropsType<typeof get
     <DefaultTemplate>
       <>
         <Layout>
+
           <TitleLogo>
             <Logo />
           </TitleLogo>
+
           <TitleImg>
             <Image src={'/' + posts[0].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
           </TitleImg>
+
           <TitleText>
             <TitleLarge>もっと、暮らしを、□ □ □ 。</TitleLarge>
             <TitleSmall>green があれば、今日の生活はどうなるだろう？</TitleSmall>
           </TitleText>
+
         </Layout>
+
         <Layout>
-          <Wrapper1>
-            <Wrapper1Content>
-              <Image src={'/' + posts[1].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[2].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[3].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[4].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[5].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[6].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[7].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[8].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[9].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-          </Wrapper1>
-        </Layout>
-        <Layout>
-          <Wrapper2Main>
-            <Wrapper2MainImg>
-              <Image src={'/' + posts[10].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper2MainImg>
-            <Wrapper2MainText>ほらほらほら<br />こうやっておくと<br />超かわいい！</Wrapper2MainText>
-          </Wrapper2Main>
-          <Wrapper2Sub>
-            <Image src={'/' + posts[11].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-          </Wrapper2Sub>
-          <Wrapper2Comment>なんだかすてきな Life になる気がしてきた</Wrapper2Comment>
-        </Layout>
-        <Layout>
-          <Wrapper3Img1>
-            <Image src={'/' + posts[12].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-          </Wrapper3Img1>
-          <Wrapper3Img2>
-            <Image src={'/' + posts[13].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-          </Wrapper3Img2>
-          <Wrapper3Img3>
-            <Image src={'/' + posts[14].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-          </Wrapper3Img3>
-          <Wrapper3Img4>
-            <Image src={'/' + posts[15].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-          </Wrapper3Img4>
-        </Layout>
-        <Layout>
-          <Wrapper1>
-            <Wrapper1Content>
-              <Image src={'/' + posts[16].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[17].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[18].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[19].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[20].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[21].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[22].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[23].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-            <Wrapper1Content>
-              <Image src={'/' + posts[24].imagePath} alt="サンプル画像" height="600" width="600" objectFit="cover" />
-            </Wrapper1Content>
-          </Wrapper1>
+          <Contents>
+            {posts.map((post => {
+              return(
+                <ContentImg>
+                  <Image src={'/' + post.imagePath} alt={post.comment} height="600" width="600" objectFit="cover" />
+                </ContentImg>
+              )
+            }))}
+          </Contents>
         </Layout>
       </>
     </DefaultTemplate>
@@ -143,70 +71,22 @@ const TitleLarge = styled.p`
 const TitleSmall = styled.p`
   font-size: 14px;
 `;
-const Wrapper1 = styled.div`
+const Contents = styled.div`
   padding: 0 16px;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
 `;
-const Wrapper1Content = styled.div`
+const ContentImg = styled.div`
   width: calc( calc(100% - 32px) / 3);
   margin-bottom: 16px;
-`;
-const Wrapper2Main = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 80px;
-`;
-const Wrapper2MainImg = styled.div`
-  width: 50%;
-`;
-const Wrapper2MainText = styled.p`
-  padding: 0 24px;
-  font-size: 14px;
-  width: 50%;
-  line-height: 2;
-`;
-const Wrapper2Sub = styled.div`
-  padding: 0 0 0 40px;
-`;
-const Wrapper2Comment = styled.p`
-  margin: 0 auto;
-  text-align: center;
-  padding: 80px 0 0;
-`;
-const Wrapper3Img1 = styled.div`
-  text-align: center;
-  padding: 0 0 0 80px;
-  margin: 0 auto 36px;
-  width: 240px;
-`;
-const Wrapper3Img2 = styled.div`
-  text-align: center;
-  padding: 0 80px 0 0;
-  margin: 0 auto 36px;
-  width: 240px;
-`;
-const Wrapper3Img3 = styled.div`
-  text-align: center;
-  padding: 0 0 0 80px;
-  margin: 0 0 36px auto;
-  width: 240px;
-`;
-const Wrapper3Img4 = styled.div`
-  text-align: center;
-  padding: 0 40px;
-  width: 240px;
-  margin: 0 auto;
 `;
 
 
 export const getStaticProps: GetStaticProps = async () => {
 
-  const query = new URLSearchParams({ page : '1', count: '25' });
+  const query = new URLSearchParams({ page : '1', count: '27' });
   const url = `${process.env.NEXT_PUBLIC_API_URL}posts?${query}`;
 
   const { page, posts } = await (await fetch(url)).json();

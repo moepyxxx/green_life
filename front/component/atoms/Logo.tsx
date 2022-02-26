60a175a5b6a9ad89be364eb7c0c89cb261d9cacd
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-02-26 17:12:03 +0900

    ロゴへリンクをつけた

--- front/component/atoms/Logo.tsx
+++ front/component/atoms/Logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import LogoDefault from '../../img/logo/default.svg'
 import LogoWhite from '../../img/logo/white.svg'
@@ -9,7 +10,12 @@ type Props = {
   color?: 'default' | 'white'
 }
 
-const Logo: React.FC<Props> = ( { color = 'default' } ) =>
-  (<Image src={ color === 'default' ? LogoDefault : LogoWhite} alt="ロゴ" />);
+const Logo: React.FC<Props> = ( { color = 'default' } ) => {
+  return (
+    <Link href="/" passHref>
+      <Image src={ color === 'default' ? LogoDefault : LogoWhite} alt="ロゴ" />
+    </Link>
+  );;
 
+}
 export default Logo

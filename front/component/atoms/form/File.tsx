d7d3dc28825937d7a3949388288e1d9bce8ed87c
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-03-04 14:05:38 +0900

    画像登録APIを処理できるようにした

---
+++ front/component/atoms/form/File.tsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react'
+import styled from 'styled-components';
+import SquareButton from '../../molecules/SquareButton';
+
+type Props = {
+  setSelectFlag: () => void
+  setImageFile: (file: File) => void
+}
+const File: React.FC<Props> = ( { setSelectFlag, setImageFile } ) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const click = () => {
+    inputRef.current?.click();
+  };
+
+  const change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectFlag();
+    setImageFile(e.target.files[0]);
+  }
+
+  return (
+    <>
+
+      <FormParts type="file" onChange={change} ref={inputRef} hidden placeholder="このグリーン、とっても素敵でしょう！" accept="image/jpeg, image/png"  />
+      <SquareButton click={click}>ここへ選択してね</SquareButton>
+    </>
+  );
+}
+
+export default File
+
+const FormParts = styled.input`
+  display: none;
+`;

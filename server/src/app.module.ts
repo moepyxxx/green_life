4e9524873cbb02efdaa63d07eb80d09345f7d6a3
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-03-05 09:47:37 +0900

    ストレージに保存して返却できるようにした

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostModule } from './posts/post.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PostModule } from './posts/post.module';
+import { ImageModule } from './images/image.module';
 
 const url = `mongodb://root:example@mongo:27017/greenlife?authSource=admin`;
 
 @Module({
   imports: [
     MongooseModule.forRoot(url),
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local'],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'image'),
     }),
-    PostModule
+    PostModule,
+    ImageModule,
   ],
   controllers: [AppController],
   providers: [AppService],

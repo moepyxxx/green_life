ccd65548cc5abc3ea6184f2e03b9f0678efb395b
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-03-13 14:08:20 +0900

    useカスタムフックの使い方ミスってたため修正した

--- front/utility/customhooks/useFetch.ts
+++ front/utility/customhooks/useFetch.ts
@@ -1,16 +1,26 @@
 import axios from "axios";
 import useFetchUser from "./usefetchUser";
+import useLogout from "./useLogout";
 
-const useFetch = async <T>(apiPath: string, isRequiredAuthorization?: boolean): Promise<T> => {
+const useFetch = () => {
 
-  if (isRequiredAuthorization) {
-    return await (await axios.get(process.env.NEXT_PUBLIC_API_LOCAL_URL + apiPath, {
-      headers: { Authorization: `Bearer ${useFetchUser().token}`}
-    })).data;
+  const logout = useLogout();
 
-  } else {
-    return await (await axios.get(process.env.NEXT_PUBLIC_API_LOCAL_URL + apiPath)).data;
-
+  return async <T>(apiPath: string, isRequiredAuthorization?: boolean): Promise<T> => {
+    try {
+      if (isRequiredAuthorization) {
+        return await (await axios.get(process.env.NEXT_PUBLIC_API_LOCAL_URL + apiPath, {
+          headers: { Authorization: `Bearer ${useFetchUser().token}`}
+        })).data;
+    
+      } else {
+        return await (await axios.get(process.env.NEXT_PUBLIC_API_LOCAL_URL + apiPath)).data;
+      }
+      
+    } catch(_) {
+      logout();
+    }
   }
+
 }
 export default useFetch;
---
+++ front/utility/customhooks/useLogout.ts
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export const useLogout = (): () => void => {
+  const router = useRouter();
+
+  return () => {
+    localStorage.removeItem('jwt');
+    router.push('/signin?type=timeout')
+  }
+}
+export default useLogout;

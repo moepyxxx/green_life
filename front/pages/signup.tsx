744759f2b7c1466058c16c7e396fb1f2c20216c4
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-03-13 11:49:58 +0900

    サインアップの不要なコードを削除した

--- front/pages/signup.tsx
+++ front/pages/signup.tsx
@@ -1,5 +1,4 @@
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Logo from '../component/atoms/Logo';
 import GreenLifeDescription from '../component/features/auth/GreenLifeDescription';
@@ -8,8 +7,6 @@ import SignupPanel from '../component/features/auth/SignupPanel';
 import DefaultTemplate from '../component/templates/Default';
 
 export default function SignUp() {
-
-  const router = useRouter();
 
   return (
     <DefaultTemplate>
@@ -29,36 +26,4 @@ export default function SignUp() {
 const ItemLogo = styled.div`
   margin: 100px auto 60px;
   width: 200px;
-`;
-
-
-const Title = styled.div`
-  margin-top: 76px;
-  text-align: center;
-
-  img {
-    width: 180px;
-  }
-`;
-
-const TitleLogo = styled.div`
-  width: 180px;
-  margin: 0 auto 20px;
-`;
-
-const Contents = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-between;
-  position: relative;
-  &:after {
-    content: '';
-    display: block;
-    width: calc( calc(100% - 32px) / 3);
-  }
-`;
-const ContentImg = styled.div`
-  width: calc( calc(100% - 32px) / 3);
-  margin-bottom: 16px;
 `;
